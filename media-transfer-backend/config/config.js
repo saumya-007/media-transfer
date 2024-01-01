e56f1f552b8f8f5c5df13b32c4058c3d604e5451
saumya <saumya.dixit@rapidops.com>
2024-01-01 18:43:04 +0530

    removing unused config

--- media-transfer-backend/config/config.js
+++ media-transfer-backend/config/config.js
@@ -3,9 +3,4 @@ module.exports.serverConfig = {
   defaultHost: 'http://localhost'
 }
 
-module.exports.cockroachCloudCluster = {
-  connectionString: 'postgresql://lsm-admin:Atyw-4zvtwbQB7gl4rfKSg@lms-project-2473.7s5.cockroachlabs.cloud:26257/playlistconverter_db?sslmode=verify-full',
-  dbName: 'playlistconverter_db'
-}
-
-module.exports.localFolderPath = '/home/ad.rapidops.com/saumya.dixit/Server Storage';
+module.exports.localFolderPath = '<YOUR_ROOT_FOLDER_PATH>';
